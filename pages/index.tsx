07d86ac0616fed60f6ced3d539e3091c2f377273
techguylhk <techguylinhtutkyaw@gmail.com>
2021-11-16 16:21:42 +0630

    added Headless and Hero icons removed fontawesome

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,15 +1,17 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import type { NextPage } from 'next';
+import Head from 'next/head';
 
 const Home: NextPage = () => {
   return (
     <>
       <Head>
+        <meta charSet='utf-8' />
         <title>Title Here</title>
       </Head>
-      <div className="text-green-500">
-        Body Here <i className="fas fa-user"></i>
-      </div>
+      <body>
+        <noscript>You need to enable JavaScript to run this app.</noscript>
+        Hello World
+      </body>
     </>
   );
 };
